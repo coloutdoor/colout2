ad0ef324d914363a5e57436a4d1cb56da040694a
Deron Jensen <deron@columbiaoutdoor.com>
2025-02-22 20:32:41 -0800

    Added godotenv and --def flags to allow for localhost binding

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"html/template"
 	"net/http"
+	"os"
 	"strconv"
+
+	_ "github.com/joho/godotenv/autoload"
 )
 
 type DeckEstimate struct {
@@ -84,10 +88,24 @@ func estimateHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	devMode := flag.Bool("dev", false, "Run in development mode (localhost only)")
+	flag.Parse()
+
 	http.HandleFunc("/", homeHandler)
 	http.HandleFunc("/estimate", estimateHandler)
-	fmt.Println("Server starting on :8080...")
-	err := http.ListenAndServe(":8080", nil)
+	//fmt.Println("Server starting on :8080...")
+	// err := http.ListenAndServe(":8080", nil)
+	addr := ":8080"
+	if envAddr := os.Getenv("SERVER_ADDR"); envAddr != "" {
+		addr = envAddr
+		fmt.Printf("Server starting on %s (from env)...\n", addr)
+	} else if *devMode {
+		addr = "127.0.0.1:8080"
+		fmt.Println("Server starting on localhost:8080 (dev mode)...")
+	} else {
+		fmt.Println("Default Server starting on :8080...")
+	}
+	err := http.ListenAndServe(addr, nil)
 	if err != nil {
 		fmt.Println("Error starting server:", err)
 	}
