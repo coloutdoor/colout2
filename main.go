3072405d932bf5b717a253295218cfa497a5ed4c
Deron Jensen <deron@columbiaoutdoor.com>
2025-02-22 19:54:29 -0800

    Added height to structure and output

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 type DeckEstimate struct {
 	Length    float64
 	Width     float64
+	Height    float64
 	Material  string
 	TotalCost float64
 	Error     string
@@ -27,6 +28,11 @@ func estimateHandler(w http.ResponseWriter, r *http.Request) {
 		tmpl.Execute(w, nil)
 		return
 	}
+	height, err := strconv.ParseFloat(r.FormValue("height"), 64)
+	if err != nil || height < 0 {
+		tmpl.Execute(w, DeckEstimate{Error: "Height must be a non-negative number"})
+		return
+	}
 
 	length, _ := strconv.ParseFloat(r.FormValue("length"), 64)
 	width, _ := strconv.ParseFloat(r.FormValue("width"), 64)
@@ -35,6 +41,7 @@ func estimateHandler(w http.ResponseWriter, r *http.Request) {
 	estimate := DeckEstimate{
 		Length:   length,
 		Width:    width,
+		Height:   height,
 		Material: material,
 	}
 
@@ -57,7 +64,22 @@ func estimateHandler(w http.ResponseWriter, r *http.Request) {
 		tmpl.Execute(w, estimate)
 		return
 	}
-	estimate.TotalCost = area * costPerSqFt
+	// estimate.TotalCost = area * costPerSqFt
+
+	baseCost := area * costPerSqFt
+
+	// Height adjustment
+	if height >= 20 {
+		estimate.Error = "We can't build decks 20 feet or higher without additional information."
+		tmpl.Execute(w, estimate)
+		return
+	} else if height > 4 {
+		excessHeight := height - 4
+		multiplier := 1 + (excessHeight * 0.01) // 1% per foot over 4
+		estimate.TotalCost = baseCost * multiplier
+	} else {
+		estimate.TotalCost = baseCost
+	}
 	tmpl.Execute(w, estimate)
 }
 
