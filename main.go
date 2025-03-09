2ef3c9b73cc20e18d98e0043b53400f027bd53de
Deron Jensen <deron@columbiaoutdoor.com>
2025-03-09 15:23:21 -0700

    Add auto population from session for Deck estimate data

--- main.go
+++ main.go
@@ -14,8 +14,22 @@ import (
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	// tmpl := template.Must(template.ParseFiles("templates/index.html"))
 	// tmpl := template.Must(template.ParseFiles("templates/index.html").Funcs(funcMap))
-	tmpl := template.Must(template.New("index").Funcs(funcMap).ParseFiles("templates/index.html"))
-	if err := tmpl.ExecuteTemplate(w, "index.html", nil); err != nil {
+	tmpl := template.Must(template.New("index.html").Funcs(funcMap).ParseFiles("templates/index.html"))
+
+	// Get session
+	session, err := store.Get(r, "colout2-session")
+	if err != nil {
+		log.Printf("Session get error: %v", err)
+		http.Error(w, "Session error", http.StatusInternalServerError)
+		return
+	}
+
+	// Load estimate from session
+	estimate := DeckEstimate{}
+	if est, ok := session.Values["estimate"].(DeckEstimate); ok {
+		estimate = est
+	}
+	if err := tmpl.ExecuteTemplate(w, "index.html", estimate); err != nil {
 		log.Printf("homeHandler execute error: %v", err)
 		panic(err)
 	}
